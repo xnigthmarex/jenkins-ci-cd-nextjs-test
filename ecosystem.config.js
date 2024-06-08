79358a54f5117624acaf1b70c1436544eb8ff171
Aryan Singh <135762633+xnigthmarex@users.noreply.github.com>
2024-06-07 23:53:43 -0500

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    apps: [
+      {
+        name: "nextjs-app",
+        script: "npm",
+        args: "start",
+        cwd: "/var/jenkins_home/workspace/TEST",
+        env: {
+          NODE_ENV: "production"
+        }
+      }
+    ]
+  };
+  
